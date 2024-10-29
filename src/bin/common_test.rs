62cae80466a8ac731aac31d6ddcf99ab4d10d194
aseuler <aseuler@outlook.com>
2024-10-29 19:35:59 +0800

    update

--- src/bin/common_test.rs
+++ src/bin/common_test.rs
@@ -1,24 +1,24 @@
-use common;
+use webbase;
 
 #[tokio::main]
 pub async fn main() {
-    let current_day = common::db_datatime::get_current_day();
+    let current_day = webbase::db_datatime::get_current_day();
     println!("current_day: {:?}", current_day);
 
-    let current_datetime = common::db_datatime::get_current_time_east_8_str();
+    let current_datetime = webbase::db_datatime::get_current_time_east_8_str();
     println!("current_datetime: {:?}", current_datetime);
 
-    println!("{:?}", common::common::get_uuid_string_without_minus());
-    println!("{:?}", common::common::get_uuid());
-    println!("{:?}", common::common::get_uuid_string());
+    println!("{:?}", webbase::common::get_uuid_string_without_minus());
+    println!("{:?}", webbase::common::get_uuid());
+    println!("{:?}", webbase::common::get_uuid_string());
 
-    common::keycenter::keycenter_check_health(&"http://49.235.147.196:6013/".into())
+    webbase::keycenter::keycenter_check_health(&"http://49.235.147.196:6013/".into())
         .await
         .unwrap();
 
-    let result = common::keycenter::keycenter_encode_token(
+    let result = webbase::keycenter::keycenter_encode_token(
         &"http://49.235.147.196:6013/".into(),
-        common::keycenter::JwtEncodeReq {
+        webbase::keycenter::JwtEncodeReq {
             account: "test".into(),
             name: "testname".into(),
             appid: "onetwo".into(),
@@ -30,7 +30,7 @@ pub async fn main() {
 
     println!(
         "{:?}",
-        common::keycenter::keycenter_docode_token(
+        webbase::keycenter::keycenter_docode_token(
             &"http://49.235.147.196:6013/".into(),
             result.access
         )
